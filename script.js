76c8f0a5d733dffdbc6a1c6956ce02fe9f17bcdd
nocoineratall <andrears90@hotmail.it>
2024-04-08 19:00:40 +0200

    Adjust password match functionality to enable or disable submit button

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const submitBtn = document.querySelector(".submit-btn");
 const warnMsg = document.querySelector(".pwd-check-message");
-const pwds = document.querySelector(".password");
+const pwd1 = document.querySelector("#password1");
+const pwd2 = document.querySelector("#password2");
 
-//fix this!
-pwds.forEach((pwd) => {
-  if (pwd.value != undefined && pwd2.value == pwd1.value) {
+pwd1.addEventListener("focusout", () => {
+  if (pwd1.value != undefined && pwd1.value == pwd2.value) {
     warnMsg.textContent = "";
-    submitBtn.setAttribute("disabled", "ciao");
+    submitBtn.disabled = false;
   } else {
+    submitBtn.disabled = true;
+    let warning = "*Passwords do not match";
+    warnMsg.textContent = warning;
+  }
+});
+
+pwd2.addEventListener("focusout", () => {
+  if (pwd2.value != undefined && pwd1.value == pwd2.value) {
+    warnMsg.textContent = "";
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
     let warning = "*Passwords do not match";
     warnMsg.textContent = warning;
   }
